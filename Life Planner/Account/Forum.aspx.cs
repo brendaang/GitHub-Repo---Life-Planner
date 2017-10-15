1b1e06fe85256734e5b5eb1b33979e674174b0e3
Brenda Ang Ting Le <brenda.ang95@outlook.com>
2017-10-15 23:38:52 +0800

    Forum (Done): - Create New Threads - View Threads - Post to Thread - View Posts - Posts Paging - Vulgarity Filter Forum (Not Done): -Editing -Deleting -Reporting -Liking -Disliking
    I have made changes to the database too. The script will be on google drive.
    
    ~Brenda

---
+++ Life Planner/Account/Forum.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Life_Planner.Data;
+
+namespace Life_Planner.Account
+{
+    public partial class Forum : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable ViewThreadsTable = new DataTable();
+            SqlConnection con = new DBManager().getConnection();
+            string sql = "SELECT t.[threadID],t.[threadName],t.[threadDesc],a.[userName],t.[dateCreated] FROM [CZ2006 - Life Planner].[dbo].[Threads] t INNER JOIN [CZ2006 - Life Planner].[dbo].[Account] a ON t.accID = a.accID;";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ViewThreadsTable);
+            threadsGridView.DataSource = ViewThreadsTable;
+            threadsGridView.DataBind();
+            con.Close();
+        }
+
+        //GridView selecting
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchFunction();
+        }
+
+        protected void searchFunction()
+        {
+            //string tName ;
+            DataTable ViewThreadsTable = new DataTable();
+            SqlConnection con = new DBManager().getConnection();
+            string sql = "SELECT t.[threadID],t.[threadName],t.[threadDesc],a.[userName],t.[dateCreated] FROM [CZ2006 - Life Planner].[dbo].[Threads] t INNER JOIN [CZ2006 - Life Planner].[dbo].[Account] a ON t.accID = a.accID WHERE t.[threadName] LIKE '%' + @pSearch + '%';";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.Add("@pSearch", SqlDbType.NVarChar).Value = tbSearch.Text;
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ViewThreadsTable);
+            threadsGridView.DataSource = ViewThreadsTable;
+            threadsGridView.DataBind();
+            con.Close();
+        }
+
+        protected void threadsGridView_SelectedIndexChanging(object sender, EventArgs e)
+        {
+            //set the select index
+
+            searchFunction();
+
+            int intThreadID = (int)threadsGridView.DataKeys[threadsGridView.SelectedIndex].Value;
+            string threadID = intThreadID.ToString();
+            lblThreadID.Text = threadID;
+
+            //save threadID into session
+            Session["ThreadID"] = threadID;
+
+            //Response.Redirect("Posts-View.aspx");
+        }
+
+        protected void getPosts(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "viewPosts")
+            {
+                //searchFunction();
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = threadsGridView.Rows[index];
+
+                string threadID = row.Cells[0].Text;
+                Session["ThreadID"] = threadID;
+                Response.Redirect("Posts.aspx", false);
+            }
+        }
+        protected void getAuthor(object sender, EventArgs e)
+        {
+            LinkButton lb = (LinkButton)sender;
+            string authorName = lb.Text;
+            Session["AuthorName"] = authorName;
+            //Response.Redirect("~/ViewProfiles.aspx");
+        }
+
+        protected void createThread_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateThread.aspx");
+        }
+    }
+}
---
+++ Life Planner/Data/DBManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Life_Planner.Data
+{
+    public class DBManager
+    {
+        public SqlConnection getConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["CZ2006 - Life PlannerConnectionString"].ConnectionString);
+        }
+    }
+}
