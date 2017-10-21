4d65e0630be2ef64439096bce51b452140ffb303
Brenda Ang Ting Le <brenda.ang95@outlook.com>
2017-10-21 23:45:15 +0800

    21/10/17 Edited search function of forum

--- Life Planner/Account/Forum.aspx.cs
+++ Life Planner/Account/Forum.aspx.cs
@@ -39,7 +39,7 @@ namespace Life_Planner.Account
             //string tName ;
             DataTable ViewThreadsTable = new DataTable();
             SqlConnection con = new DBManager().getConnection();
-            string sql = "SELECT t.[threadID],t.[threadName],t.[threadDesc],a.[userName],t.[dateCreated] FROM [CZ2006 - Life Planner].[dbo].[Threads] t INNER JOIN [CZ2006 - Life Planner].[dbo].[Account] a ON t.accID = a.accountID WHERE t.[threadName] LIKE '%' + @pSearch + '%';";
+            string sql = "SELECT t.[threadID],t.[threadName],t.[threadDesc],a.[username],t.[dateCreated] FROM [CZ2006 - Life Planner].[dbo].[Threads] t INNER JOIN [CZ2006 - Life Planner].[dbo].[AccCreds] a ON t.accID = a.accountID WHERE t.[threadName] LIKE '%' + @pSearch + '%';";
             SqlCommand cmd = new SqlCommand(sql, con);
             cmd.Parameters.Add("@pSearch", SqlDbType.NVarChar).Value = tbSearch.Text;
             con.Open();
