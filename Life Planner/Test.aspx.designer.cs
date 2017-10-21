bc8416366886858cf9ec798a09fb8e83dacec94f
Cheejyg <glenncheejunyuan@hotmail.com>
2017-10-21 11:30:26 +0800

    Revert "Merge branch 'master' of https://github.com/brendaang/GitHub-Repo---Life-Planner"
    This reverts commit 536cb2aeea8e50b310140fb7df307f8e93a132c6, reversing
    changes made to 5284c41dd1e0b39af37ce5f150f825a3af1c1f42.

--- Life Planner/Test.aspx.designer.cs
+++ Life Planner/Test.aspx.designer.cs
@@ -22,20 +22,12 @@ namespace Life_Planner {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
-<<<<<<< HEAD
         /// S1 control.
-=======
-        /// Table1 control.
->>>>>>> f6e423c137678bfc045b63a33542ac7b34eee90a
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-<<<<<<< HEAD
         protected global::System.Web.UI.WebControls.TextBox S1;
-=======
-        protected global::System.Web.UI.WebControls.Table Table1;
->>>>>>> f6e423c137678bfc045b63a33542ac7b34eee90a
     }
 }
