5de0b092f87f0ae3a316e104bf14bf8893085766
Cheejyg <glenncheejunyuan@hotmail.com>
2017-10-21 12:40:41 +0800

    Added attributes, methods...
    Added attributes, methods...

--- Life Planner/Module.cs
+++ Life Planner/Module.cs
@@ -6,5 +6,52 @@ using System.Threading.Tasks;
 
 namespace Life_Planner {
 	class Module {
+		private int moduleID;
+		private string moduleName;
+		private double moduleCost, moduleDuration;
+		private string moduleRequirement;
+
+		public Module() {
+			moduleID = -1;
+			moduleName = null;
+			moduleCost = -1;
+			moduleDuration = -1;
+			moduleRequirement = null;
+		}
+
+		public int getModuleID() {
+			return moduleID;
+		}
+		public void setModuleID(int moduleID) {
+			this.moduleID = moduleID;
+		}
+		public string getModuleName() {
+			return moduleName;
+		}
+		public void setModuleName(string moduleName) {
+			this.moduleName = moduleName;
+		}
+		public double getModuleCost() {
+			return moduleCost;
+		}
+		public void setModuleCost(double moduleCost) {
+			this.moduleCost = moduleCost;
+		}
+		public double getModuleDuration() {
+			return moduleDuration;
+		}
+		public void setModuleDuration(double moduleDuration) {
+			this.moduleDuration = moduleDuration;
+		}
+		public string getModuleRequirement() {
+			return moduleRequirement;
+		}
+
+		public double calculateLongestTime() {
+			return -1;
+		}
+		public double calculateShortestTime() {
+			return -1;
+		}
 	}
 }
